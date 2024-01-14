0c462081abfc47a7c7f10d2f0fb5ef9c2b44c756
HabibaHefnyy <97574320+Habiba93@users.noreply.github.com>
2024-01-14 21:49:33 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
+const Blockchain = require("./blockchain");
+let egycoin = new Blockchain();
+egycoin.createGenesisBlock();
 
+egycoin.createNewTransaction(8000,"karim","habiba");
+
+egycoin.createNewTransaction(8000,"haidy","menna");
+egycoin.createNewTransaction(8000,"nour","ahmed");
+
+console.log(egycoin);
+
+//console.log(Date);
